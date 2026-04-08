3d6422440a5bad255fe9b697dd7a4ebe1740e069
Alin V. (Claudiu) <alin.voinea@gmail.com>
2026-04-08 11:17:52 +0300

    chore: Volto 18 eslint/jest migration - refs #301881 (volto-quote-block)

--- src/Blocks/Quote/Edit.test.jsx
+++ src/Blocks/Quote/Edit.test.jsx
@@ -23,14 +23,14 @@ jest.mock('@plone/volto/components/manage/Sidebar/SidebarPortal', () => {
   return ({ children }) => <div data-testid="sidebar-portal">{children}</div>;
 });
 
-jest.mock('@plone/volto/components/manage/Form', () => ({
-  BlockDataForm: (props) => (
+jest.mock('@plone/volto/components/manage/Form/BlockDataForm', () => {
+  return (props) => (
     <div data-testid="block-data-form">
       <p>Mocked BlockDataForm</p>
       <input data-testid="block-data-input" onChange={props.onChangeField} />
     </div>
-  ),
-}));
+  );
+});
 
 const mockStore = configureStore([]);
 
